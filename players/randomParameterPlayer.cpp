f47c6e1584ca6a7729a3e78a02bd658d39c773c5
YukiFujimoto <tdmhk1@gmail.com>
2015-12-21 22:12:00 +0900

    ランダムにパラメータを決めるプレイヤーを実装

---
+++ players/randomParameterPlayer.cpp
@@ -0,0 +1,9 @@
+#include <random>
+#include <iostream>
+
+std::random_device rnd;
+
+double territoryMerits = rnd();
+double selfTerritoryMerits = rnd();
+double hurtingMerits = rnd();
+double hidingMerits = rnd();
