2d92ac55720105484c64eb75109e0fac1e60c823
YukiFujimoto <tdmhk1@gmail.com>
2016-01-08 11:50:34 +0900

    余分なファイルを削除しました

--- testmatch.sh
+++ testmatch.sh
@@ -2,8 +2,8 @@
 manager/gameManager \
     -a players/greedyPlayer -p "" -u "" -n "greedy1" -r 1 -s 100 \
     -a players/greedyPlayer -p "" -u "" -n "greedy2" -r 2 -s 98 \
-    -a players/randomPlayer -p "" -u "" -n "greedy3" -r 3 -s 80 \
-    -a players/nonCheckingPlayer -p "" -u "" -n "random4" -r 4 -s 70 \
-    -a players/tooActivePlayer -p "" -u "" -n "random5" -r 5 -s 60 \
-    -a players/abortingPlayer  -p "" -u "" -n "random6" -r 6 -s 20 \
+    -a players/greedyPlayer -p "" -u "" -n "greedy3" -r 3 -s 80 \
+    -a players/greedyPlayer -p "" -u "" -n "greedy4" -r 4 -s 70 \
+    -a players/greedyPlayer -p "" -u "" -n "greedy5" -r 5 -s 60 \
+    -a players/greedyPlayer  -p "" -u "" -n "greedy6" -r 6 -s 20 \
     -l logfiles/logfile11 \
